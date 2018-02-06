72dedb12685ede04620fb15c1dfbbf3cbb1b199b
Richard Valverde <ricvr98@gmail.com>
2018-02-06 14:19:15 -0600

    Dejamos de usar la libreria Bluetooth y volvimos a trabajar con el puerto serial

--- luces/luces.ino
+++ luces/luces.ino
@@ -1,37 +1,64 @@
-#include <SoftwareSerial.h>
-SoftwareSerial BT1(4,2);//RX, TX
 int ledpin;
+int pin_inicial=2;
+int pin_final=10;
+
+void reset ()
+{
+  int i;
+  for(i=pin_inicial;i<pin_final;i++)
+  {
+    digitalWrite(i,LOW);
+  }
+}
 
 void admin_valor(int pin , char(t))
 {
-  if(pin > 4 && pin < 13)
+  int i;
+  if(pin >= 0 && pin < 10)
   {
     ledpin==pin;
-    Serial.print(int(ledpin));
   }
   pin -= 48;
   
   if(t=='h' || t=='H')
   {
     digitalWrite(pin,HIGH);
-    //Serial.println(String(t));
   }
   if(t=='l' || t=='L')
   {
     digitalWrite(pin,LOW);
-    //Serial.println(String(t));
   }
-  
+  if(pin==0)
+  {
+    reset();
+  }
+  else if(pin==1)
+  {
+    reset();
+    for(i=pin_inicial;i<pin_final;i++)
+    {
+      digitalWrite(i,HIGH);
+      delay(300);
+      digitalWrite(i,LOW); 
+      delay(300);
+    }
+    for(i=pin_final;i>=pin_inicial;i--)
+    {
+      digitalWrite(i,HIGH);
+      delay(300);
+      digitalWrite(i,LOW); 
+      delay(300);
+    }
+  }
 }
 
 void captura()
 {
-  if(BT1.available())
+  if(Serial.available()==null)
   {
-    Serial.write(BT1.read());
-    //delay(500);
+    delay(100);
   }
-  if(Serial.available())
+  if(Serial.available()!=null)
   {
     int pin = Serial.read();
     char estado= Serial.read();
@@ -44,7 +71,7 @@ void captura()
 
 void setup()
 {
-  //int i;
+  pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
@@ -52,14 +79,7 @@ void setup()
   pinMode(7,OUTPUT);
   pinMode(8,OUTPUT);
   pinMode(9,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
   Serial.begin(9600);
-  //Serial.println("Esto es CS50x.ni");
-  //delay(1000);
-  BT1.begin(9600);
 }
 void loop()
 {
